32ef48e7c2f663d5d4b4d327c658215f063f116f
NiallMacDermott <niall.macdermott@ark-energy.eu>
2018-02-27 13:15:49 +0000

    v1.1.1

---
+++ dist/helpers.js
@@ -0,0 +1,68 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.callNextComplete = exports.callComplete = exports.callError = exports.callNext = exports.validateUser = exports.validateToken = exports.Failures = exports.fromNullable = undefined;
+
+var _daggy = require("daggy");
+
+var _rxjs = require("rxjs");
+
+var _Either = require("crocks/Either");
+
+var _Either2 = _interopRequireDefault(_Either);
+
+var _isNil = require("crocks/predicates/isNil");
+
+var _isNil2 = _interopRequireDefault(_isNil);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Left = _Either2.default.Left,
+    Right = _Either2.default.Right;
+var fromNullable = exports.fromNullable = function fromNullable(x) {
+  return (0, _isNil2.default)(x) ? Left(x) : Right(x);
+};
+
+var Failures = exports.Failures = (0, _daggy.taggedSum)("Failures", {
+  Callback: ["error"],
+  SSO: [],
+  Expired: []
+});
+
+var validateToken = exports.validateToken = function validateToken(exp) {
+  return _rxjs.Observable.create(function (obs) {
+    if (Date.now() > exp - 10000) {
+      return obs.error(Failures.Expired);
+    }
+    obs.next(exp);
+    return obs.complete();
+  });
+};
+
+var validateUser = exports.validateUser = function validateUser(user) {
+  return validateToken(user.expiresAt || 0).mapTo(user);
+};
+
+var callNext = exports.callNext = function callNext(obj) {
+  return function (val) {
+    return obj.next(val);
+  };
+};
+var callError = exports.callError = function callError(obj) {
+  return function (val) {
+    return obj.error(val);
+  };
+};
+var callComplete = exports.callComplete = function callComplete(obj) {
+  return function () {
+    return obj.complete();
+  };
+};
+var callNextComplete = exports.callNextComplete = function callNextComplete(obj) {
+  return function (val) {
+    obj.next(val);
+    obj.complete();
+  };
+};
---
+++ dist/index.js
@@ -0,0 +1,129 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = CreateAuthModule;
+
+var _auth0Js = require("auth0-js");
+
+var _ramda = require("ramda");
+
+var _rxjs = require("rxjs");
+
+var _either = require("crocks/pointfree/either");
+
+var _either2 = _interopRequireDefault(_either);
+
+var _helpers = require("./helpers");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function CreateAuthModule(_ref) {
+  var clientID = _ref.clientID,
+      domain = _ref.domain,
+      redirectUri = _ref.redirectUri,
+      apiAudience = _ref.apiAudience;
+
+  var auth0 = new _auth0Js.WebAuth({
+    audience: apiAudience,
+    domain: domain,
+    clientID: clientID,
+    scope: "openid profile email",
+    redirectUri: redirectUri,
+    responseType: "token"
+  });
+
+  var parseHash = _rxjs.Observable.create(function (obs) {
+    return auth0.parseHash(function (err, payload) {
+      var errCheck = (0, _helpers.fromNullable)(err);
+      var payloadCheck = (0, _helpers.fromNullable)(payload);
+
+      errCheck.swap((0, _ramda.always)(payloadCheck), _helpers.Failures.Callback).either((0, _helpers.callError)(obs), (0, _helpers.callNextComplete)(obs));
+    });
+  });
+
+  var checkSession = function checkSession() {
+    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    return _rxjs.Observable.create(function (obs) {
+      return auth0.checkSession(options, function (err, result) {
+        return (0, _helpers.fromNullable)(err).swap((0, _ramda.always)(result), (0, _ramda.always)(_helpers.Failures.SSO)).map(function (res) {
+          return (0, _ramda.merge)(res, { expiresAt: Date.now() + result.expiresIn * 1000 });
+        }).either((0, _helpers.callError)(obs), (0, _helpers.callNextComplete)(obs));
+      });
+    });
+  };
+
+  var getUserInfo = function getUserInfo(result) {
+    return _rxjs.Observable.create(function (obs) {
+      return auth0.client.userInfo(result.accessToken, function (err, profile) {
+        (0, _helpers.fromNullable)(err).swap((0, _ramda.always)(result), (0, _ramda.always)(result)).map(function (user) {
+          return (0, _ramda.merge)(user, profile);
+        }).either((0, _helpers.callError)(obs), (0, _helpers.callNextComplete)(obs));
+      });
+    }).catch(_rxjs.Observable.of);
+  };
+
+  var authenticate = checkSession().flatMap(getUserInfo);
+
+  var getUserFromStorage = _rxjs.Observable.create(function (obs) {
+    var user = JSON.parse(localStorage.getItem("user")) || {};
+    obs.next(user);
+    obs.complete();
+  });
+
+  var storeUser = function storeUser(user) {
+    return _rxjs.Observable.create(function (obs) {
+      localStorage.setItem("user", JSON.stringify(user));
+
+      obs.next(user);
+      obs.complete();
+    });
+  };
+
+  var initialize = parseHash.flatMap((0, _either2.default)(function () {
+    return getUserFromStorage;
+  }, getUserInfo)).flatMap(_helpers.validateUser).catch(function (err) {
+    return err.cata({
+      Callback: (0, _ramda.compose)(_rxjs.Observable.throw, _helpers.Failures.Callback),
+      SSO: function SSO() {
+        return _rxjs.Observable.throw(_helpers.Failures.SSO);
+      },
+      Expired: function Expired() {
+        return authenticate;
+      }
+    });
+  }).flatMap(storeUser);
+
+  var maintainLogin = initialize.flatMap(function (user) {
+    return _rxjs.Observable.empty().delay(new Date(user.expiresAt - 10000)).startWith(user);
+  }).repeatWhen(function (x) {
+    return x;
+  });
+
+  function logout(_ref2) {
+    var returnUrl = _ref2.returnUrl;
+
+    var logoutFn = auth0.logout.bind(auth0, { returnTo: returnUrl });
+    storeUser({}).subscribe(logoutFn);
+  }
+
+  function login() {
+    auth0.authorize({
+      redirectUri: redirectUri
+    });
+  }
+
+  // () as placeholder for audience
+  var getToken = function getToken() {
+    return getUserFromStorage.map((0, _ramda.prop)("access_token"));
+  };
+
+  return {
+    initialize: initialize,
+    logout: logout,
+    login: login,
+    maintainLogin: maintainLogin,
+    getToken: getToken
+  };
+}
