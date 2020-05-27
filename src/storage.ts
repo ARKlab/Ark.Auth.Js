59553cccbbe9cee916d72c556379749facd44138
NiallMacDermott <niall.macdermott@ark-energy.eu>
2020-05-27 13:11:22 +0100

    add-oidc

--- src/storage.ts
+++ src/storage.ts
@@ -1,6 +1,8 @@
 import { IO } from "fp-ts/lib/IO";
 import { User } from "./types";
 import { fromNullable, some, none, Option } from "fp-ts/lib/Option";
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/pipeable";
 
 type StoredUser = User & { userVersion?: string };
 const userVersion = "v1";
@@ -11,44 +13,46 @@ export type AuthStorage = {
   clearUser: IO<void>;
 };
 export const createLocalStorage = (key = "user"): AuthStorage => ({
-  getUser: new IO(() =>
-    fromNullable(localStorage.getItem(key))
-      .map(usr => JSON.parse(usr) as StoredUser)
-      .chain(user =>
-        fromNullable(user.userVersion).chain(version =>
-          version === userVersion ? some(user) : none
+  getUser: () =>
+    pipe(
+      fromNullable(localStorage.getItem(key)),
+      O.map((usr) => JSON.parse(usr) as StoredUser),
+      O.chain((user) =>
+        pipe(
+          fromNullable(user.userVersion),
+          O.chain((version) => (version === userVersion ? some(user) : none))
         )
       )
-  ),
-  setUser: (value: User) =>
-    new IO(() =>
-      localStorage.setItem(
-        key,
-        JSON.stringify({ ...value, userVersion: userVersion })
-      )
     ),
-  clearUser: new IO(() => localStorage.removeItem(key))
+
+  setUser: (value: User) => () =>
+    localStorage.setItem(
+      key,
+      JSON.stringify({ ...value, userVersion: userVersion })
+    ),
+  clearUser: () => localStorage.removeItem(key),
 });
 
 export const createInMemoryStorage = (): AuthStorage => {
   let user: string | undefined = undefined;
 
   return {
-    getUser: new IO(() =>
-      fromNullable(user)
-        .map(usr => JSON.parse(usr) as StoredUser)
-        .chain(user =>
-          fromNullable(user.userVersion).chain(version =>
-            version === userVersion ? some(user) : none
+    getUser: () =>
+      pipe(
+        fromNullable(user),
+        O.map((usr) => JSON.parse(usr) as StoredUser),
+        O.chain((user) =>
+          pipe(
+            fromNullable(user.userVersion),
+            O.chain((version) => (version === userVersion ? some(user) : none))
           )
         )
-    ),
-    setUser: (value: User) =>
-      new IO(() => {
-        user = JSON.stringify({ ...value, userVersion: userVersion });
-      }),
-    clearUser: new IO(() => {
+      ),
+    setUser: (value: User) => () => {
+      user = JSON.stringify({ ...value, userVersion: userVersion });
+    },
+    clearUser: () => {
       user = undefined;
-    })
+    },
   };
 };
